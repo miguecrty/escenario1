03f01b9cf60121463ecd0d1e3eb48151f2c288c2
Migue <miguelangelloppez7@gmail.com>
2023-11-21 18:11:32 +0100

    salida

--- servidor/servidor.py
+++ servidor/servidor.py
@@ -8,21 +8,30 @@ server_socket.listen(1)
 print(f"Servidor escuchando en {HOST}:{PORT}")
 print("Esperando conexion...\n")
 
-while True:
-    conn, addr = server_socket.accept()
-    with conn:
-        print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀')
-        print(f"Conexión establecida desde {addr}")
-        print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀   CHAT   ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀')
-        print('     Para salir del chat escribe \'salir\'     ')
-        print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n\n')
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(f"			Recibido: {data.decode()}\n")
-            if data.decode() == 'salir':
-                break
-            # Ahora el servidor también puede enviar mensajes al cliente
-            mensaje_servidor = input(": ")
-            conn.sendall(mensaje_servidor.encode())
+# Initialize salida as a global variable
+salida = False
+
+def main():
+    global salida
+    while not salida:
+        conn, addr = server_socket.accept()
+        with conn:
+            print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀')
+            print(f"Conexión establecida desde {addr}")
+            print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀   CHAT   ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀')
+            print('     Para salir del chat escribe \'salir\'     ')
+            print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n\n')
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(f"			Recibido: {data.decode()}\n")
+                if data.decode() == 'salida':
+                    salida = True
+                # Ahora el servidor también puede enviar mensajes al cliente
+                mensaje_servidor = input(": ")
+                conn.sendall(mensaje_servidor.encode())
+
+if __name__ == "__main__":
+    main()
+
