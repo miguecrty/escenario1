9a48fcd731085411cf9090798154d07d20d66e65
Migue <miguelangelloppez7@gmail.com>
2023-11-22 11:23:59 +0100

    escenario1

--- servidor/servidor.py
+++ servidor/servidor.py
@@ -5,33 +5,35 @@ PORT = 8080
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen(1)
+print('─────────────────────────────────────────────────────────────────')
+print('                           ESCENARIO 1                           ')
+print('─────────────────────────────────────────────────────────────────')
+print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀')
 print(f"Servidor escuchando en {HOST}:{PORT}")
 print("Esperando conexion...\n")
 
-# Initialize salida as a global variable
-salida = False
-
-def main():
-    global salida
-    while not salida:
-        conn, addr = server_socket.accept()
-        with conn:
-            print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀')
-            print(f"Conexión establecida desde {addr}")
-            print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀   CHAT   ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀')
-            print('     Para salir del chat escribe \'salir\'     ')
-            print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n\n')
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                print(f"			Recibido: {data.decode()}\n")
-                if data.decode() == 'salida':
-                    salida = True
-                # Ahora el servidor también puede enviar mensajes al cliente
-                mensaje_servidor = input(": ")
-                conn.sendall(mensaje_servidor.encode())
-
-if __name__ == "__main__":
-    main()
-
+while True:
+    conn, addr = server_socket.accept()
+    with conn:
+        print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀')
+        print(f"Conexión establecida desde {addr}")
+        print('────────────────────────────   CHAT   ───────────────────────────')
+        print('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n\n')
+        
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            if data.decode() == 'salir':
+                print("Se ha perdido la conexión.")
+                break
+            else:
+                print("\t\t\t\t────────────────────────────────")
+                print(f"\t\t\t\t| Recibido: {data.decode()}")
+                print("\t\t\t\t────────────────────────────────")
+            print("──────────────────────────")
+            mensaje_servidor = input("|Tú: ")
+            print("──────────────────────────")
+            conn.sendall(mensaje_servidor.encode())
+        conn.close()
+        break
